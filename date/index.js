251509eec675e4d02767a724be2226337cc3133d
himaana <himana.on@gmail.com>
2025-12-15 19:46:09 +0700

    feat: completed task 2

--- date/index.js
+++ date/index.js
@@ -6,5 +6,43 @@
 
     Show it using alert.
 */
-let date = new Date(Date.parse("2012-02-20T03:12-07:00"));
+let date = new Date(Date.parse("2012-02-20T03:12"));
 alert(date);
+
+/* 
+    Task 2
+    Show a weakday
+
+    Write a function getWeekDay(date) to show the weekday in short format: ‘MO’, ‘TU’, ‘WE’, ‘TH’, ‘FR’, ‘SA’, ‘SU’.
+*/
+function getWeekDay(date) {
+    switch (date.getDay()) {
+        case 1:
+            return "MO";
+            break;
+        case 2:
+            return "TU";
+            break;
+        case 3:
+            return "WE";
+            break;
+        case 4:
+            return "TH";
+            break;
+        case 5:
+            return "FR";
+            break;
+        case 6:
+            return "SA";
+            break;
+        case 0:
+            return "SU";
+            break;
+        default:
+            break;
+    }
+}
+
+date = new Date(2012, 0, 3);
+console.log(date);
+console.log(getWeekDay(date));
