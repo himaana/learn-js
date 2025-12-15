4c1d7b54bacd6d96c989b8d2eb239ee999bf6bce
himaana <himana.on@gmail.com>
2025-12-15 19:48:47 +0700

    refactor: change solution for task 2 with array

--- date/index.js
+++ date/index.js
@@ -16,33 +16,10 @@ alert(date);
     Write a function getWeekDay(date) to show the weekday in short format: ‘MO’, ‘TU’, ‘WE’, ‘TH’, ‘FR’, ‘SA’, ‘SU’.
 */
 function getWeekDay(date) {
-    switch (date.getDay()) {
-        case 1:
-            return "MO";
-            break;
-        case 2:
-            return "TU";
-            break;
-        case 3:
-            return "WE";
-            break;
-        case 4:
-            return "TH";
-            break;
-        case 5:
-            return "FR";
-            break;
-        case 6:
-            return "SA";
-            break;
-        case 0:
-            return "SU";
-            break;
-        default:
-            break;
-    }
+    let days = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+
+    return days[date.getDay()];
 }
 
 date = new Date(2012, 0, 3);
-console.log(date);
 console.log(getWeekDay(date));
