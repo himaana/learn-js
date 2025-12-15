086da5b58e4ce9fa97a97bf5d5845798d03a5ed4
himaana <himana.on@gmail.com>
2025-12-15 19:33:47 +0700

    feat: completed task 1

---
+++ date/index.js
@@ -0,0 +1,10 @@
+/* 
+    Task 1
+    Create a date
+
+    Create a Date object for the date: Feb 20, 2012, 3:12 am. The time zone is local.
+
+    Show it using alert.
+*/
+let date = new Date(Date.parse("2012-02-20T03:12-07:00"));
+alert(date);
