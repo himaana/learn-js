f3cf96e5ea3db8c083ea59d4c49e7b27b6b9f16e
himaana <himana.on@gmail.com>
2025-12-15 17:22:20 +0700

    feat: completed task 2

--- destructuring-assignment/index.js
+++ destructuring-assignment/index.js
@@ -19,3 +19,43 @@ let { name, years: age, isAdmin = false } = user;
 console.log(name);
 console.log(age);
 console.log(isAdmin);
+
+/* 
+    Task 2
+    The maximal salary
+    
+    There is a salaries object:
+
+    let salaries = {
+            "John": 100,
+            "Pete": 300,
+            "Marcy": 250
+    };
+
+    Create the function topSalary(salaries) that returns the name of the top-paid person.
+    • if salaries is empty, it should return null.
+    • if there are multiple top-paid persons, return any of them.
+
+    P.S. Use Object.entries() and destructuring to iterate over key/values pairs.
+*/
+function topSalary(salaries) {
+    let topPerson = null;
+    let topPersonSalary = 0;
+
+    for (const [key, value] of Object.entries(salaries)) {
+        if (topPersonSalary < value) {
+            topPerson = key;
+            topPersonSalary = value;
+        }
+    }
+
+    return topPerson;
+}
+
+let salaries = {
+    John: 100,
+    Pete: 300,
+    Marcy: 250,
+};
+
+console.log(topSalary(salaries));
