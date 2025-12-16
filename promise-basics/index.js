a03e6342e61c41016db110fe9dd5985482b3621b
himaana <himana.on@gmail.com>
2025-12-16 17:21:24 +0700

    refactor: delete reject callback

--- promise-basics/index.js
+++ promise-basics/index.js
@@ -7,11 +7,7 @@
     The function delay(ms) should return a promise. That promise should resolve after ms miliseconds, so that we can add .then to it.
 */
 function delay(ms) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(() => {
-            resolve();
-        }, ms);
-    });
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 delay(3000).then(() => console.log("runs after 3 seconds"));
