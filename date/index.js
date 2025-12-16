9b07770d45dd32475b5c5d54261acfad21a7b16e
himaana <himana.on@gmail.com>
2025-12-16 08:02:27 +0700

    feat: completed task 3

--- date/index.js
+++ date/index.js
@@ -23,3 +23,22 @@ function getWeekDay(date) {
 
 date = new Date(2012, 0, 3);
 console.log(getWeekDay(date));
+
+/* 
+    Task 3
+    European weekday
+
+    European countries have days of week starting with Monday (nummber 1), then Tuesday (number 2) and till Sunday (number 7).
+
+    Write a function getLocalDay(date) that returns the "European" day of week for date.
+*/
+
+function getLocalDay(date) {
+    if (date.getDay() === 0) {
+        return 7;
+    }
+    return date.getDay();
+}
+
+date = new Date(2012, 0, 3);
+alert(getLocalDay(date));
