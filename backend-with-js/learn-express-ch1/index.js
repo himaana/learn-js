0c663c6f230812d041e57ffae07483b6e84cf51a
himaana <himana.on@gmail.com>
2025-12-18 10:04:29 +0700

    feat: add 2 new routes (about and contact)

--- backend-with-js/learn-express-ch1/index.js
+++ backend-with-js/learn-express-ch1/index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
     res.send("Hello from Express");
 });
 
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Contact Page");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
